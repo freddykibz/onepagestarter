a994b3c1a4709dbff24c3eb439c888a404b21b8f
Fred kibuthu <frdkinyua@gmail.com>
2022-06-29 19:12:37 +0300

    initail commit

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+
+import 'react-multi-carousel/lib/styles.css';
+import 'react-modal-video/css/modal-video.min.css';
+import 'rc-drawer/assets/index.css';
+import '@fontsource/dm-sans';
+
+export default function CustomApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+
